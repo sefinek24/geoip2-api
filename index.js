0b2855963d9654a480480b189cfaeea99eaf9aab
Sefinek <92880265+sefinek24@users.noreply.github.com>
2023-10-08 17:33:33 +0200

    v0.1.3

--- index.js
+++ index.js
@@ -1,5 +1,5 @@
 const https = require('node:https');
-const { name, version } = require('./package.json');
+const { name, version, devDependencies } = require('./package.json');
 
 // Define the base URL and API path for the GeoIP service.
 const API_BASE_URL = 'api.sefinek.net';
@@ -7,14 +7,18 @@ const API_PATH = '/api/v2/geoip/';
 
 // Define the headers for the HTTP request.
 const headers = {
-	'User-Agent': `${name}/${version} (+https://github.com/sefinek24/geoip2-api)`,
+	'User-Agent': `${name}/${version} (+https://github.com/sefinek24/geoip2-api) ${process.env.JEST_WORKER_ID === undefined ? '' : `jest/${devDependencies.jest.replace('^', '')}`}`,
 	'Accept': 'application/json',
-	'Accept-Language': 'en-US,en;q=0.9',
 	'Cache-Control': 'no-cache',
-	'Pragma': 'no-cache',
-	'DNT': '1',
+	'CF-IPCountry': 'false',
+	'CF-Visitor': '{"scheme":"https"}',
 	'Connection': 'keep-alive',
-	'Upgrade-Insecure-Requests': '1',
+	'DNT': '1',
+	'Pragma': 'no-cache',
+	'Referrer-Policy': 'strict-origin-when-cross-origin',
+	'X-Content-Type-Options': 'nosniff',
+	'X-Frame-Options': 'DENY',
+	'X-XSS-Protection': '1; mode=block',
 };
 
 /**
@@ -41,8 +45,7 @@ function makeRequest(ip) {
 
 			res.on('end', () => {
 				try {
-					const response = JSON.parse(data);
-					resolve(response);
+					resolve(JSON.parse(data));
 				} catch (err) {
 					reject(err);
 				}
@@ -58,6 +61,4 @@ function makeRequest(ip) {
 }
 
 // Export the makeRequest function for external use.
-module.exports = {
-	get: makeRequest,
-};
+module.exports = { get: makeRequest };
