382882bac1bc9720289c8817be090fa2fb210072
Sefinek <contact@sefinek.net>
2023-12-24 04:22:38 +0100

    Update

--- example.js
+++ example.js
@@ -1,7 +1,28 @@
 const geoIp = require('./index.js');
-const address = '185.244.214.231';
+const address = '86.63.119.32';
 
 (async () => {
 	const data = await geoIp.get(address);
 	console.log(data);
 })();
+
+// Output:
+//
+// {
+//  success: true,
+//  status: 200,
+//  validationErr: false,
+//  message: 'Found',
+//  ip: '86.63.119.32',
+//  data: {
+//    range: [ 1446998016, 1447000063 ],
+//    country: 'PL',
+//    region: '30',
+//    eu: '1',
+//    timezone: 'Europe/Warsaw',
+//    city: 'Pila',
+//    ll: [ 53.1492, 16.7461 ],
+//    metro: 0,
+//    area: 5
+//  }
+// }
