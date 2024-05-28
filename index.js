cde25f983dac43c5338603a15e84b3582a754e95
Sefinek <contact@sefinek.net>
2024-05-28 15:25:06 +0200

    Quality fixes

--- index.js
+++ index.js
@@ -1,9 +1,9 @@
 const https = require('https');
 const { name, version, devDependencies } = require('./package.json');
 
-// Define the headers for the HTTP request
+// HTTP request headers
 const headers = {
-	'User-Agent': `${name}/${version} (+https://github.com/sefinek24/geoip2-api) ${!process.env.JEST_WORKER_ID ? '' : `jest/${devDependencies.jest.replace(/^[^0-9]*/, '')}`}`,
+	'User-Agent': `${name}/${version} (+https://github.com/sefinek24/geoip2-api)${process.env.JEST_WORKER_ID ? ` jest/${devDependencies.jest.replace(/^[^0-9]*/, '')}` : ''}`,
 	'Accept': 'application/json',
 	'Cache-Control': 'no-cache',
 	'CF-Visitor': '{"scheme":"https"}',
@@ -16,33 +16,31 @@ const headers = {
 	'X-XSS-Protection': '1; mode=block'
 };
 
-// Main function with timeout
-const makeRequest = ip => {
-	return new Promise((resolve, reject) => {
-		const req = https.get(`https://api.sefinek.net/api/v2/geoip/${ip}`, { headers }, (res) => {
-			if ((res.statusCode < 200 || res.statusCode >= 300) && res.statusCode !== 404) {
-				return reject(new Error(`HTTP Status Code: ${res.statusCode}`));
+// Makes a GET request to the GeoIP API and returns the response
+const makeRequest = ip => new Promise((resolve, reject) => {
+	const req = https.get(`https://api.sefinek.net/api/v2/geoip/${ip}`, { headers }, res => {
+		if ((res.statusCode < 200 || res.statusCode >= 300) && res.statusCode !== 404) {
+			return reject(new Error(`HTTP Status Code: ${res.statusCode}`));
+		}
+
+		const chunks = [];
+		res.on('data', chunk => chunks.push(chunk));
+		res.on('end', () => {
+			try {
+				const data = Buffer.concat(chunks).toString();
+				resolve(JSON.parse(data));
+			} catch (err) {
+				reject(err);
 			}
-
-			const chunks = [];
-			res.on('data', chunk => chunks.push(chunk));
-			res.on('end', () => {
-				try {
-					const data = Buffer.concat(chunks).toString();
-					resolve(JSON.parse(data));
-				} catch (err) {
-					reject(err);
-				}
-			});
-		});
-
-		req.on('error', reject);
-		req.setTimeout(7500, () => {
-			req.destroy();
-			reject(new Error('Timeout error'));
 		});
 	});
-};
 
-// Export the makeRequest function for external use
+	req.on('error', reject);
+	req.setTimeout(7500, () => {
+		req.destroy();
+		reject(new Error('Timeout error'));
+	});
+});
+
+// Exports the makeRequest function and version for external use
 module.exports = { get: makeRequest, version };
