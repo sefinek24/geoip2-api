cbd8f6d857b224965a07cbc14afd35a4b055a57b
Sefinek <contact@sefinek.net>
2024-07-30 18:38:45 +0200

    Update docs

--- index.js
+++ index.js
@@ -11,7 +11,7 @@ const headers = {
 	'DNT': '1'
 };
 
-// Makes a GET request to the GeoIP API and returns the response
+// Sends a GET request to the Sefinek API and returns the response
 const makeRequest = ip => new Promise((resolve, reject) => {
 	const req = https.get(`https://api.sefinek.net/api/v2/geoip/${ip}`, { headers, timeout: 20000 }, res => {
 		if ((res.statusCode < 200 || res.statusCode >= 300) && res.statusCode !== 400) {
@@ -37,5 +37,5 @@ const makeRequest = ip => new Promise((resolve, reject) => {
 	});
 });
 
-// Exports the makeRequest function and version for external use
+// Exports the makeRequest function and the module version for external use
 module.exports = { get: makeRequest, version };
